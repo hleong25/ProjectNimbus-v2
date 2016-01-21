91be6ea95a01bd47d94e5ccf21ba336f4d02a6a5
henry <hleong25@gmail.com>
2016-01-20 16:16:50 -0800

    Initial submit with gradle files and main file

---
+++ NimbusApp/src/main/java/com/lahenry/test/Greetings.java
@@ -0,0 +1,10 @@
+package com.lahenry.test;
+
+public class Greetings {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
